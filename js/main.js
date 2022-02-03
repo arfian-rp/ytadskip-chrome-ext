08c854c435a7328b15689ae1108ec22cbc8854af
arfian-rp <arfianpradana07@gmail.com>
2022-02-03 18:32:18 +0700

    ok

---
+++ js/main.js
@@ -0,0 +1,7 @@
+setInterval(() => {
+  const skipBtn = document.getElementsByClassName("ytp-ad-skip-button");
+  if (skipBtn != undefined && skipBtn.length > 0) {
+    console.log(`ad detect`);
+    skipBtn[0].click();
+  }
+}, 100);
